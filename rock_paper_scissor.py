7b8dafba8eab10f05a02c96bb32326153cf39488
Tejas Singh <tejassingh74823@gmail.com>
2026-01-02 23:34:44 +0530

    Rock-Paper_Scissor game in python

---
+++ rock_paper_scissor.py
@@ -0,0 +1,55 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+scissor = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+def print_choice(a):
+    if a == "rock":
+        print(rock)
+    elif a == "paper":
+        print(paper)
+    elif a == "scissor":
+        print(scissor)
+
+import random
+print("Welcome to Rock-Paper-Scissors")
+user_choice = input("What do you choose? (Rock / Paper / Scissor): ").lower()
+comp_choice = random.choice(["rock","paper","scissor"])
+
+if user_choice == comp_choice:
+    print(f"You chose: {user_choice}")
+    print_choice(user_choice)
+    print(f"Computer chose: {comp_choice}")
+    print_choice(comp_choice)
+    print("Its a tie")
+elif ((user_choice == "rock" and comp_choice == "scissor") or (user_choice == "paper" and comp_choice == "rock") or (user_choice == "scissor" and comp_choice == "paper")):
+    print(f"You chose: {user_choice}")
+    print_choice(user_choice)
+    print(f"Computer chose: {comp_choice}")
+    print_choice(comp_choice)
+    print("Congratulation!!\nYou WON!!!")
+else:
+    print(f"You chose: {user_choice}")
+    print_choice(user_choice)
+    print(f"Computer chose: {comp_choice}")
+    print_choice(comp_choice)
+    print("Computer won")
